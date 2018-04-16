0af00e631bfc5237f55edfb25ec7cca2edafe45a
Eik Hunter <eikhunter@Flys-iMac.local>
2018-04-16 11:44:53 +0100

    Initial setup and build

---
+++ src/js/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <p>Header Item 1</p>
+      <p>Header Item 2</p>
+      <p>Header Item 3</p>
+    </div>
+  )
+}
+
+export default Header
