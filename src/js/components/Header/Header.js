65f3af619d14c87e9366238f48336d4c0d0d7be3
Eik Hunter <eikhunter@Flys-iMac.local>
2018-04-16 13:25:42 +0100

    Add base header component

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import logo from '../../../assets/logo.png'
+import styles from './Header.scss'
+
 const Header = () => {
   return (
-    <div>
-      <p>Header Item 1</p>
-      <p>Header Item 2</p>
-      <p>Header Item 3</p>
+    <div className="hd-Header">
+      <div className="hd-Header_Inner">
+        <div className="hd-Header_Body">
+          <a href="/" className="hd-Header_Logo">
+            <img alt="logo" src={logo}/>
+          </a>
+          <nav className="hd-Navigation">
+            <ul className="hd-Navigation_Items">
+              <li className="hd-Navigation_Item">
+                <a href="/" className="hd-Navigation_Link">List your property</a>
+              </li>
+              <li className="hd-Navigation_Item">
+                <a href="/" className="hd-Navigation_Link">Sign up / Login</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   )
 }
